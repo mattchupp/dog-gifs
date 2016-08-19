aabbeba71e091bcf4824c35d4635d886603733e8
Matthew Chupp <mattchupp@Matts-Mac-Pro.netgear.com>
2016-08-19 15:30:24 -0500

    initial commit

---
+++ bot.js
@@ -0,0 +1,101 @@
+// Twitter Bot for tweeting dog gifs
+// Created by matt chupp
+
+// the things i need
+var fs = require('fs');
+var Twit = require('twit');
+var config = require('./config');
+
+var T = new Twit(config);
+
+// set up user stream
+// var stream = T.stream('user');
+
+// anytime someone follows me follow them back
+// stream.on('tweet', tweetEvent);
+//
+// function tweetEvent(event) {
+//   var replyto = event.in_reply_to_screen_name;
+//   var text = event.text;
+//   var fr = event.user.screen_name;
+//
+//   if (replyto == 'helloitsme541') {
+//     var newTweet = '@' + fr + ' thank you for tweeting at me!';
+//     tweetIt(newTweet);
+//   }
+// }
+
+// folder name for images
+var img = 'img/';
+
+var gifs_array = [
+  img + 'pugs.gif',
+  img + 'corgi_dive'
+];
+
+var status_array = [
+  'pugs running',
+  'diving in'
+]
+
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// tweet
+// tweetIt();
+// setInterval(tweetIt, 1000*30);
+
+// var gifToTweet = 'img/pugs.gif';
+// var alt = 'Pugs running';
+// var status = 'Running Pugs'
+// img/corgi_dive.gif
+
+var i = 0;
+function increment(i) {
+  i++;
+  return i;
+}
+
+setInterval(function() {tweetGif(gifs_array[i], status_array[i], status_array[i])}, 1000*20);
+
+// function to tweet the gifs
+function tweetGif(gif, alternateText, tweetStatus) {
+  var b64content = fs.readFileSync(gif, { encoding: 'base64' })
+
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    // now we can assign alt text to the media, for use by screen readers and
+    // other text-based presentations and interpreters
+    var mediaIdStr = data.media_id_string
+    var altText = alternateText
+    var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+    T.post('media/metadata/create', meta_params, function (err, data, response) {
+      if (!err) {
+        // now we can reference the media and post a tweet (media will attach to the tweet)
+        var params = { status: tweetStatus, media_ids: [mediaIdStr] }
+
+        T.post('statuses/update', params, function (err, data, response) {
+          console.log(data)
+        })
+        increment(i);
+      } else {
+        console.log('Houston we have a problem.');
+      }
+    })
+  })
+}
+
+// function for tweeting
+function tweetIt(txt) {
+  // tweet
+  var tweet = {
+    status: txt
+  }
+
+  T.post('statuses/update', tweet , tweeted);
+
+  function tweeted(err, data, response) {
+    console.log(data)
+  }
+}
---
+++ config.js
@@ -0,0 +1,7 @@
+// keys for connecting to twitter account
+module.exports = {
+    consumer_key:         'rrXKzscwohfUdSzknwk7uxhyq',
+    consumer_secret:      'mA3vLjTtuIkxOM2xRvUFS7K6GOGw4xvCJ9ptql7trBlBglIv2u',
+    access_token:         '766679187792990208-yKK5K8Thhy8SuySNcj69EM5d3AhOEL8',
+    access_token_secret:  '6EhS3lxty7uSbC8m0ENx4UqB9sOGdHUNQLfWZfxaiHXdr'
+}
